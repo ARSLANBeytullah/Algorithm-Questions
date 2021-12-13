734aa6d2f4338f262d8158d8d18e6a2da09affa0
ARSLANBeytullah <86007327+ARSLANBeytullah@users.noreply.github.com>
2021-12-13 17:47:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10, number2 = 20;
+            Console.WriteLine(number1 + number2);
+        }
+    }
+}
